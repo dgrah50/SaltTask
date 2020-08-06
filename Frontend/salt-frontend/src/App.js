defc1193516a6b84b8d73f4406b7dd4a6643ab1b
dgrah50 <d.graham50@hotmail.co.uk>
2020-08-06 19:42:53 +0100

    create product card on homepage

--- Frontend/salt-frontend/src/App.js
+++ Frontend/salt-frontend/src/App.js
@@ -1,47 +1,52 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import HomePage from "./pages/Home/index.js"
-import CartPage from "./pages/Cart/index.js"
+import HomePage from "./pages/Home/index.js";
+import CartPage from "./pages/Cart/index.js";
 
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/home">Home Page</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart Page</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/cart">
-            <CartPage />
-          </Route>
-          {/* <PrivateRoute path="/protected">
-            <ProtectedPage />
-          </PrivateRoute> */}
-        </Switch>
-      </div>
+      <NavBar />
+      <Switch>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/cart">
+          <CartPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 export default App;
 
+function NavBar() {
+  const barStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+    flex: 1,
+    width: "100%",
+  };
+  return (
+    <div style={barStyle}>
+      <ul>
+        <li>
+          <Link to="/home">Home Page</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart Page</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 function About() {
   return <h2>About</h2>;
--- Frontend/salt-frontend/src/pages/Home/index.js
+++ Frontend/salt-frontend/src/pages/Home/index.js
@@ -1,20 +1,87 @@
-
 //Dependencies
-import React, { Component } from 'react';
-
-// import map from 'lodash/map';
-//Internals
-// import './index.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000/products", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ products: data });
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }
 
   render() {
-    return(
-      <div>
-        <h1>This is the HomePage</h1>
+    return (
+      <div style={containerStyle}>
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <h1>All Items</h1>
+        </div>
+
+        {this.state.products.map((product, idx) => productCard(product, idx))}
       </div>
     );
   }
 }
+
+const containerStyle = {
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  flex: 4,
+};
+
+const productCard = (product, idx) => {
+
+  const BORDER_RADIUS = 10
+  const wrapperStyle = {
+    width: "20%",
+    padding: 20,
+  };
+  const cardStyle = {
+    backgroundColor: "#FF",
+    flexGrow: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: BORDER_RADIUS,
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  };
+  const imageStyle = {
+    borderTopLeftRadius: BORDER_RADIUS,
+    borderTopRightRadius: BORDER_RADIUS,
+    width: "100%",
+    height: 200,
+    objectFit: "cover",
+  };
+  return (
+    <div style={wrapperStyle} key={idx}>
+      <div style={cardStyle} key={idx}>
+        <img src={product.image_url} style={imageStyle} />
+        <h3>{product.product_name}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
