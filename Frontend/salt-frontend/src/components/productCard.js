1f8c1df056111b1aacafb8a2a2857897c3ccec48
dgrah50 <d.graham50@hotmail.co.uk>
2020-08-06 22:21:35 +0100

    add empty state to cart page

--- Frontend/salt-frontend/src/components/productCard.js
+++ Frontend/salt-frontend/src/components/productCard.js
@@ -16,36 +16,19 @@ const ProductCard = (product, idx) => {
         <ShoppingCartOutlined key="shopping" />,
       ]}
     >
-      <Meta title={product.product_name} description="www.instagram.com" />
+      <Meta title={product.product_name} description={product.creator} />
     </Card>
   );
 };
 
 const BORDER_RADIUS = 10;
-const wrapperStyle = {
-  width: "20%",
-  padding: 20,
-};
-const cardStyle = {
-  backgroundColor: "#FF",
-  flexGrow: 1,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  borderRadius: BORDER_RADIUS,
-  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1)",
-};
+
 const imageStyle = {
   borderTopLeftRadius: BORDER_RADIUS,
   borderTopRightRadius: BORDER_RADIUS,
   width: "100%",
   height: 200,
   objectFit: "cover",
-};
-
-const captionStyle = {
-  textAlign: "center",
 };
 
 export default ProductCard;
--- Frontend/salt-frontend/src/pages/Cart/index.js
+++ Frontend/salt-frontend/src/pages/Cart/index.js
@@ -1,20 +1,51 @@
 
 //Dependencies
 import React, { Component } from 'react';
-// import map from 'lodash/map';
-//Internals
-// import './index.css';
+import ProductCard from "../../components/productCard.js";
+
 
 class CartPage extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: [],
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000/cart", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ cart: data });
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }
 
   render() {
     return (
       <div style={containerStyle}>
-        <h1>This is the cart</h1>
+        <h1>Shopping Basket</h1>
+        <div style={productWrapperStyle}>
+          {this.state.cart.length>0 ? (this.state.cart.map((product, idx) =>
+            ProductCard(product, idx)
+          )) : (<EmptyState/>)}
+        </div>
       </div>
     );
   }
+}
+
+const EmptyState = () => {
+  return(
+    <h3>
+      Add something to your cart!
+    </h3>
+  )
 }
 
 const containerStyle = {
@@ -24,6 +55,16 @@ const containerStyle = {
   flexDirection: "row",
   flexWrap: "wrap",
   flex: 4,
+};
+
+const productWrapperStyle = {
+  paddingLeft: "10%",
+  paddingRight: "10%",
+  width: "100%",
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "center",
 };
 
 export default CartPage;
